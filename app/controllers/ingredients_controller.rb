3071809a263900b7404fa1022754cfbaabf45dc0
Bruno Toledo <brunogalizzi@gmail.com>
2020-05-03 12:41:26 -0300

    Upload files

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -23,5 +23,17 @@ class IngredientsController < ApplicationController
 
   def index; end
 
-  def cad; end
+  def cad
+    @ingredient = Ingredient.new
+  end
+
+  def post
+    @ingredient = Ingredient.create(ingredient_params)
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
 end
